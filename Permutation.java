ef5f99b636c982860911a4a3d8e41533d6d59ee8
rkpinto7 <120949051+rkpinto7@users.noreply.github.com>
2023-08-02 00:25:01 +0530

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -1,23 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
 public class Permutation {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long n = Long.parseLong(br.readLine());
-        if(n == 1) {
+ 
+        if (n == 1) {
             System.out.println(1);
-        } else if(n < 4) {
+        } else if (n < 4) {
             System.out.println("NO SOLUTION");
-        } else if(n == 4) {
+        } else if (n == 4) {
             System.out.println("2 4 1 3");
         } else {
-            for (int i = 1; i <= n; i+=2) {
-                System.out.print(i + " ");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= n; i += 2) {
+                sb.append(i); sb.append(" ");
             }
-            for (int i = 2; i <= n; i+=2) {
-                System.out.print(i + " ");
+            for (int i = 2; i <= n; i += 2) {
+                sb.append(i); sb.append(" ");
             }
+            System.out.println(sb.toString());
         }
     }
-}
